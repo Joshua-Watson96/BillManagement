4d96bb65f17508b9e32e5189373d52d394f0b0bf
Joshua Watson <joshjkwatson@gmail.com>
2023-05-29 21:31:44 +1000

    added editBill routes to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const billRoutes = require('./bill-routes');
 const categoryRoutes = require('./category-routes');
 const subcategoryRoutes = require('./subcategory-routes');
+const editBillRoutes = require('./editBill-routes')
 
 router.use('/bills', billRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/subcategories', subcategoryRoutes);
+router.use('/editBill', editBillRoutes);
 
 module.exports = router;
