8000f5258a4041e834cd27f4795174332aebdec7
eilismcmillan <eilis.mcmillan1998@gmail.com>
2023-05-31 21:02:28 +1000

    added login+logout routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+module.exports = router;
