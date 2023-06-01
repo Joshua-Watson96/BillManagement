aef9b26cb416a1812323d29cdd3da7f27716c5c5
eilismcmillan <eilis.mcmillan1998@gmail.com>
2023-06-01 15:39:30 +1000

    edited login + logout routes to fix bugs

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,18 +1,21 @@
 const router = require('express').Router();
-const { User } = require('../../models/User');
+const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await User.create({
+      name: req.body.name,
+      password: req.body.password,
+    });
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+      req.session.loggedIn = true;
 
       res.status(200).json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
@@ -45,7 +48,8 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
-    res.status(400).json(err);
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,29 +1,19 @@
 const router = require('express').Router();
-const withAuth = require('../utils/auth');
 
-router.get('/dashboard', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('dashboard', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/dashboard');
+    return;
   }
+  res.render('login');
 });
 
-// router.get('/', (req, res) => {
-//     res.render('login')
-// });
-
-router.get('/signup', (req, res) => {
-    res.render('signUp')
+router.get('/', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('signUp');
 });
 
 module.exports = router;
