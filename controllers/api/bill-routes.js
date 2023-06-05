cc810401581c6810907e74a18f7633ae627b335f
Joshua Watson <joshjkwatson@gmail.com>
2023-06-05 22:41:33 +1000

    almost working add bill

--- controllers/api/bill-routes.js
+++ controllers/api/bill-routes.js
@@ -16,18 +16,19 @@ router.get('/', async (req, res) => {
 
 //Create bill
 router.post('/', async (req, res) => {
-    try {
+    try { console.log("hi");
         const newBill = await Bill.create({
             name: req.body.name,
             due_date: req.body.due_date,
             amount: req.body.amount,
             category_id: req.body.category_id,
-            // user_id: req.session.user_id,
-            user_id: req.body.user_id
+            user_id: req.session.user_id,
+            // user_id: req.body.user_id
         });
         res.status(200).json(newBill);
     } catch (err) {
         res.status(500).json(err);
+        console.log(err);
     }
 });
 
