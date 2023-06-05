2d4be8265f69fba281ea0112e0efbb05188acc3a
eilismcmillan <eilis.mcmillan1998@gmail.com>
2023-06-05 18:47:51 +1000

    updated dashboard  to user and helper function

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Bill, Category } = require("../models");
+const { User, Bill, Category } = require("../models");
 
 router.get("/", (req, res) => {
   if (req.session.logged_in) {
@@ -17,23 +17,25 @@ router.get("/signUp", (req, res) => {
 router.get("/dashboard", async (req, res) => {
   try {
     const categoryData = await Category.findAll({
-      // where: {user_id: req.session.user_id},
       include: [
         {
           model: Bill,
           attributes: ["name", "amount", "due_date"],
+          where: {user_id: req.session.user_id},
         },
       ],
     });
     const categories = categoryData.map((category) =>
       category.get({ plain: true })
+      
     );
-
+    console.log(categories)
     const billData = await Bill.findAll({
       where: { user_id: req.session.user_id },
       order: [["due_date", "ASC"]],
     });
     const bills = billData.map((bill) => bill.get({ plain: true }));
+    console.log(bills)
     res.render("dashboard", {
       categories,
       bills,
