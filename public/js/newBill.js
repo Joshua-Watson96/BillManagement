de500adefc2d2143744c64c4101dd770d47c8ae3
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-06-06 12:01:31 +1000

    Add new bill form now works as intended. Added button to navbar to access new bill form

--- public/js/newBill.js
+++ public/js/newBill.js
@@ -1,94 +1,90 @@
-
-  // const express = require('express');
-  // const { doc } = require('prettier');
-  // const app = express();
-
-  // app.use(express.json());
-
 const addBtn = document.getElementById("addBtn");
 
 addBtn.addEventListener("click", function(event){
   event.preventDefault();
   console.log("blah blah blah");
-        const billType = document.getElementById("billTypeId").value // Gets the bill type
-        const billName = document.getElementById("billNameId").value; // Get the name value
-        const billAmount = document.getElementById("billAmountId").value; // Get the amount value
-        const billDueData = document.getElementById("billDueId").value; // Get the due date value
+  // Gets the bill type
+  const billType = document.getElementById("billTypeId").value;
+  // Get the name value
+  const billName = document.getElementById("billNameId").value;
+  // Get the amount value
+  const billAmount = document.getElementById("billAmountId").value;
+  // Get the due date value
+  const billDueData = document.getElementById("billDueId").value;
         
   
-        // add selected type to back-end with id
-        let categoryId;
-        switch (billType) {
-          case "electricity":
-            categoryId = 1;
-            break;
-          case "gas":
-            categoryId = 2;
-            break;
-          case "internet":
-            categoryId = 3;
-            break;
-          case "water":
-            categoryId = 4;
-            break;
-          case "health-insurance":
-              categoryId = 5;
-            break;
-            case "home-insurance":
-              categoryId = 6;
-            break;
-            case "car-insurance":
-              categoryId = 7;
-            break;
-            case "car-rego":
-              categoryId = 8;
-            break;
-            case "phone-bill":
-              categoryId = 9;
-            break;
-            case "school-fee":
-              categoryId = 10;
-            break;
-            case "council-rates":
-              categoryId = 11;
-            break;
-            case "dog-rego":
-              categoryId = 12;
-          default:
-            categoryId = null;
-        }
+  // add selected type to back-end with id
+  let categoryId;
+  switch (billType) {
+    case "electricity":
+      categoryId = 1;
+      break;
+    case "gas":
+      categoryId = 2;
+      break;
+    case "internet":
+      categoryId = 3;
+      break;
+    case "water":
+      categoryId = 4;
+      break;
+    case "health-insurance":
+        categoryId = 5;
+      break;
+      case "home-insurance":
+        categoryId = 6;
+      break;
+      case "car-insurance":
+        categoryId = 7;
+      break;
+      case "car-rego":
+        categoryId = 8;
+      break;
+      case "phone-bill":
+        categoryId = 9;
+      break;
+      case "school-fee":
+        categoryId = 10;
+      break;
+      case "council-rates":
+        categoryId = 11;
+      break;
+      case "dog-rego":
+        categoryId = 12;
+    default:
+      categoryId = null;
+  }
         
-        const billData = {
-          name: billName,
-          category_id: categoryId,
-          amount: billAmount,
-          due_date: billDueData,
-        }
-        console.log("blah blah", billData);
+  const billData = {
+    name: billName,
+    category_id: categoryId,
+    amount: billAmount,
+    due_date: billDueData,
+  }
 
-// adds bill to db
-        fetch('/api/bills', {
-          method: 'POST',
-          body: JSON.stringify(billData),
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          
-        })
-        .then(response => {
-          if (response.ok) {
-            console.log('Data saved successfully');
-          } else {
-            console.error('Failed to save data');
-          }
-        })
-        .catch(error => {
-          console.error('Error', error);
-        })
-      });
+  // adds bill to db
+  fetch('/api/bills', {
+    method: 'POST',
+    body: JSON.stringify(billData),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('Data saved successfully');
+    } else {
+      console.error('Failed to save data');
+    }
+  })
+  .catch(error => {
+    console.error('Error', error);
+  })
+});
 
 
-// on button click; clears all the values  
+// On button click; clears all the values  
 // Get a reference to the clear button
 const clearBtn = document.getElementById("clearBtn");
 
--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -2,13 +2,40 @@ const { Category } = require('../models');
 
 const categoryData = [
     {
-        category_name: 'Home',
+        category_name: 'Electricity',
     },
     {
-        category_name: 'Car',
+        category_name: 'Gas',
     },
     {
-        category_name: 'Insurance',
+        category_name: 'Internet',
+    },
+    {
+        category_name: 'Water',
+    },
+    {
+        category_name: 'Health Insurance',
+    },
+    {
+        category_name: 'Home Insurance',
+    },
+    {
+        category_name: 'Car Insurance',
+    },
+    {
+        category_name: 'Car Rego',
+    },
+    {
+        category_name: 'Phone Bill',
+    },
+    {
+        category_name: 'School Fee',
+    },
+    {
+        category_name: 'Council Rates',
+    },
+    {
+        category_name: 'Dog Rego',
     },
 ];
 
