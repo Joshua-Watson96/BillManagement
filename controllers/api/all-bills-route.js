2a1d41ab0972040027f17e94431b9c21ae63e2fb
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-06-06 15:51:32 +1000

    fixed formating for due-date in the categories tab

--- controllers/api/all-bills-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'); //import express
-const router  = express.Router(); 
-
-const allBillsController = require('./all-bills'); 
-
-router.post('/all-bills', allBillsController.newBill); 
-
-module.exports = router; 
--- controllers/api/all-bills.js
+++
@@ -1,5 +0,0 @@
-const newBill = (req, res, next) => {
-    res.json({message: "POST new bill"}); // dummy function for now
-};
-
-module.exports = {newBill};
